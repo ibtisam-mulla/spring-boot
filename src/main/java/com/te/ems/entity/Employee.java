b47bb7b8f239eb4f371a55b8209482752ebf29fd
ibtisam <ibtisam.mulla97@gmail.com>
2024-10-01 18:32:20 +0530

    Created Employee Entity class in entity package

---
+++ src/main/java/com/te/ems/entity/Employee.java
@@ -0,0 +1,31 @@
+package com.te.ems.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class Employee {
+    @Id
+    private String id;
+
+    @Column(unique = true)
+    private String employeeId;
+    private String firstname;
+    private String lastname;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfJoin;
+    private Double salary;
+
+    @Enumerated
+    private Gender gender;
+}
